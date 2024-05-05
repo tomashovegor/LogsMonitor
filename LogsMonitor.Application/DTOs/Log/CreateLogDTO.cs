36ba961c1714809846ccefe4f92743b479942bcc
Egor <tomashovegor@yandex.ru>
2024-05-05 18:14:49 +0500

    Добавлен UI проект на React Изменена структура проекта, API и UI выделены в отдельные папки

--- LogsMonitor.Application/DTOs/Log/CreateLogDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LogsMonitor.Application.DTOs
-{
-    public class CreateLogDTO
-    {
-        public string Message { get; set; } = null!;
-        public string Trace { get; set; } = null!;
-        public Guid ProjectId { get; set; }
-    }
-}
--- LogsMonitor.Application/DTOs/Log/LogDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LogsMonitor.Application.DTOs
-{
-    public class LogDTO
-    {
-        public Guid Id { get; set; }
-        public string Number { get; set; } = null!;
-        public DateTime OccurrenceDate { get; set; }
-    }
-}
